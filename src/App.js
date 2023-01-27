52819b3574684a7d6acab78207d6417c5f84dc97
Osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2023-01-27 12:17:45 +0100

    React Router

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import{ BrowserRouter  as Router, Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={ } />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
